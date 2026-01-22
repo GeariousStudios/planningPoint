f9f8780fff39d5611960109c340a183cc03000d7
lf-fk-admin <liam.fritzson@fresenius-kabi.com>
2026-01-22 15:08:44 +0100

    Align-top to <td>s.

--- frontend/src/app/components/manage/ManageClasses.tsx
+++ frontend/src/app/components/manage/ManageClasses.tsx
@@ -2,7 +2,7 @@ export const thClass =
   "pl-4 p-2 min-w-48 h-[40px] cursor-pointer border border-t-0 border-(--border-secondary) border-b-(--border-main) text-left transition-[background] duration-(--fast) hover:bg-(--bg-grid-header-hover)";
 
 export const tdClass =
-  "py-2 px-4 min-w-48 h-[40px] border border-b-0 border-(--border-secondary) text-left [overflow-wrap:anywhere]";
+  "py-2 px-4 min-w-48 h-[40px] border border-b-0 border-(--border-secondary) text-left [overflow-wrap:anywhere] align-top";
 
 export const filterClass =
   "truncate font-semibold transition-colors duration-(--fast) group-hover:text-(--accent-color)";
